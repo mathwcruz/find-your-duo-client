a19d4c149d8810c949989d82bbea671a3617f6dd
Matheus da Cruz <matheuswachcruz@gmail.com>
2022-09-12 21:20:48 -0300

    feat: first class finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,71 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+
+import { GameCard } from "./components/GameCard";
+
+import logo from "./assets/logo.svg";
+import "./styles/global.css";
+
 const App = () => {
   return (
-    <div>
-      <h2>Olá</h2>
+    <div className="flex my-20 flex-col max-w-[1345px] mx-auto items-center">
+      <img src={logo} alt="NLW eSports Logo" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Your{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        ir here
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <GameCard
+          imageSource="/game-1.png"
+          gameName="League of Legends"
+          adsCount={4}
+        />
+        <GameCard
+          imageSource="/game-2.png"
+          gameName="League of Legends"
+          adsCount={5}
+        />
+        <GameCard
+          imageSource="/game-3.png"
+          gameName="League of Legends"
+          adsCount={2}
+        />
+        <GameCard
+          imageSource="/game-4.png"
+          gameName="League of Legends"
+          adsCount={6}
+        />
+        <GameCard
+          imageSource="/game-5.png"
+          gameName="League of Legends"
+          adsCount={7}
+        />
+        <GameCard
+          imageSource="/game-6.png"
+          gameName="League of Legends"
+          adsCount={3}
+        />
+      </div>
+      <div className="pt-1 bg-nlw-gradient mt-8 overflow-hidden self-stretch rounded-lg">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white block font-black">
+              You didn't find your duo?
+            </strong>
+            <span className="text-base text-zinc-400 block">
+              Post an ad to find new players
+            </span>
+          </div>
+          <button
+            type="button"
+            className="hover:bg-violet-600 flex items-center gap-3 transition-colors ease-linear py-3 px-4 rounded bg-violet-500 text-white text-base"
+          >
+            <MagnifyingGlassPlus size={24} /> Publish ad
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/GameCard.tsx
@@ -0,0 +1,20 @@
+interface GameCardProps {
+  gameName: string;
+  adsCount: number;
+  imageSource: string;
+}
+
+export const GameCard = ({ imageSource, gameName, adsCount }: GameCardProps) => {
+  return (
+    <a className="relative rounded-lg overflow-hidden" href="#">
+      <img src={imageSource} alt="" />
+
+      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+        <strong className="text-base block font-bold text-white">
+          {gameName}
+        </strong>
+        <span className="text-zinc-300 block text-sm">{String(adsCount)} ads</span>
+      </div>
+    </a>
+  );
+};
